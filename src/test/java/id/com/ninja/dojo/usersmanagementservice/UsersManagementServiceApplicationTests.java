e9504411f79789126e21df6fad513b711cacd733
syamsul maarif <syamsul.maarif@ist.id>
2022-10-03 12:15:59 +0700

    initial commit
    initial commit

---
+++ src/test/java/id/com/ninja/dojo/usersmanagementservice/UsersManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.com.ninja.dojo.usersmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
